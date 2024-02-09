3a04023896b761d7d604f9c6e7147b13bcf6a9a5
GianMarcoPimentel <gianmarco.pimentel1997@hotmail.com>
2024-02-09 15:32:58 +0100

    impostazioenesercizio

---
+++ js/script.js
@@ -0,0 +1,27 @@
+/* 
+Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di domani!
+Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di lunedì mattina!
+
+
+Consigli
+Questo esercizio richiede un po' di ricerca ma anche un po' di domande che accompagnano l'uomo da tempo immemore:
+- Da quante ore è composto un giorno? (24)
+- Da quanti minuti è composta un'ora? (60)
+- Da quanti secondi è composto un minuto? (60)
+- Da quanti millisecondi è composto un secondo? (1000)
+- Quanti millisecondi mi separano da domani alle 9:30? 
+- Esiste un oggetto JS in grado di gestire le date? newDate
+- Esistono dei metodi per trasformare una data in millisecondi?
+
+
+*/
+
+
+// devo fare il conto fino a lunedi ore 9:30
+// quidni devo arrivare al 12 Febbrario 2024 ore 9:30!
+
+
+//salvo la variabile che devo raggiungere
+
+const countDate = new Date("Feb 9, 2024 9:30:00").getTime();
+
